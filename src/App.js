f99c23fb014976af36b9172dc594df4be3b7f879
Andrew Xiao <xa7144552@gmail.com>
2022-10-26 07:13:27 -0500

    Set-initial-feature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Carousel from './component/Carousel.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ maxWidth: 1200, marginLeft: 'auto', marginRight: 'auto', marginTop: 64 }}>
+      <Carousel
+        show={4}
+        // infiniteLoop={true}
+      >     
+        <div>
+          <div style={{ padding: 8 }}>
+            <img src="https://www.johnnywas.com/media/catalog/product/cache/e827a3bf21814344a7ecf8de79d925ba/l/3/l30621-8_mti_6.jpg" alt="placeholder" style={{ width: '100%' }} />
+          </div>
+          <h3 className="product-name">Dress 1</h3>
+          <p className="product-price">$800</p>
+        </div>
+        <div>
+          <div style={{ padding: 8 }}>
+            <img src="https://www.johnnywas.com/media/catalog/product/cache/e827a3bf21814344a7ecf8de79d925ba/l/3/l30621-8_mti_6.jpg" alt="placeholder" style={{ width: '100%' }} />
+          </div>
+          <h3 className="product-name">Dress 2</h3>
+          <p className="product-price">$800</p>        
+        </div>
+        <div>
+          <div style={{ padding: 8 }}>
+            <img src="https://www.johnnywas.com/media/catalog/product/cache/e827a3bf21814344a7ecf8de79d925ba/l/3/l30621-8_mti_6.jpg" alt="placeholder" style={{ width: '100%' }} />
+          </div>
+          <h3 className="product-name">Dress 3</h3>
+          <p className="product-price">$800</p>        
+        </div>
+        <div>
+          <div style={{ padding: 8 }}>
+            <img src="https://www.johnnywas.com/media/catalog/product/cache/e827a3bf21814344a7ecf8de79d925ba/l/3/l30621-8_mti_6.jpg" alt="placeholder" style={{ width: '100%' }} />
+          </div>
+          <h3 className="product-name">Dress 4</h3>
+          <p className="product-price">$800</p>
+        </div>
+        <div>
+          <div style={{ padding: 8 }}>
+            <img src="https://www.johnnywas.com/media/catalog/product/cache/e827a3bf21814344a7ecf8de79d925ba/l/3/l30621-8_mti_6.jpg" alt="placeholder" style={{ width: '100%' }} />
+          </div>
+          <h3 className="product-name">Dress 5</h3>
+          <p className="product-price">$800</p>
+        </div>
+        <div>
+          <div style={{ padding: 8 }}>
+            <img src="https://www.johnnywas.com/media/catalog/product/cache/e827a3bf21814344a7ecf8de79d925ba/l/3/l30621-8_mti_6.jpg" alt="placeholder" style={{ width: '100%' }} />
+          </div>
+          <h3 className="product-name">Dress 6</h3>
+          <p className="product-price">$800</p>
+        </div>
+        <div>
+          <div style={{ padding: 8 }}>
+            <img src="https://www.johnnywas.com/media/catalog/product/cache/e827a3bf21814344a7ecf8de79d925ba/l/3/l30621-8_mti_6.jpg" alt="placeholder" style={{ width: '100%' }} />
+          </div>
+          <h3 className="product-name">Dress 7</h3>
+          <p className="product-price">$800</p>
+        </div>
+        <div>
+          <div style={{ padding: 8 }}>
+            <img src="https://www.johnnywas.com/media/catalog/product/cache/e827a3bf21814344a7ecf8de79d925ba/l/3/l30621-8_mti_6.jpg" alt="placeholder" style={{ width: '100%' }} />
+          </div>
+          <h3 className="product-name">Dress 8</h3>
+          <p className="product-price">$800</p>
+        </div>
+      </Carousel>
+
     </div>
   );
 }
